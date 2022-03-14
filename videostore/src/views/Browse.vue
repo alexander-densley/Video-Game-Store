93a1e6fe8dd3b6aa991ebd371e53ef0ea803c71f
Alexander Densley <zander.densley@gmail.com>
2022-03-13 18:40:02 -0600

    modified data

---
+++ videostore/src/views/Browse.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <div class="pure-menu pure-menu-horizontal">
+    <ul class="pure-menu-list">
+      <li class="pure-menu-item"><a @click="select('Playstation')" href="#" class="pure-menu-link">Playstation</a></li>
+      <li class="pure-menu-item"><a @click="select('Xbox')" href="#" class="pure-menu-link">Xbox</a></li>
+      <li class="pure-menu-item"><a @click="select('Nintendo')" href="#" class="pure-menu-link">Nintendo</a></li>
+    </ul>
+  </div>
+  <ProductList :products="products" />
+</div>
+</template>
+
+<script>
+import ProductList from "../components/ProductList.vue"
+export default {
+  name: 'BrowseView',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+      platform: '',
+    }
+  },
+  computed: {
+    products() {
+      return this.$root.$data.products.filter(product => product.platform === this.platform);
+    }
+  },
+  methods: {
+    select(platform) {
+      this.platform = platform;
+    }
+  }
+}
+</script>
---
+++ videostore/src/views/CartView.vue
@@ -0,0 +1,27 @@
+<template>    
+    <div>
+        <h1>Your Cart</h1>
+        <CartList :products="cart" />
+    </div>
+    
+</template>
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'CartView',
+  components: {
+    CartList
+  },
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    }
+  }
+}
+</script>
